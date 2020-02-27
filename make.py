511454fdc526fb7b20ed3f1a01ef17a8d55e4cd8
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2020-02-27 19:16:01 +0100

    Restructured project

--- make.py
+++ make.py
@@ -3,13 +3,6 @@ import codecs
 import jinja2
 import pathlib
 
-# markdown.markdownFromFile(
-#     input="terminology.md", 
-#     output="terminology.html", 
-#     output_format="html5",
-#     tab_length=2,
-#     extensions=['def_list'])
-
 input_file = codecs.open("terminology.md", mode="r", encoding="utf-8")
 text = input_file.read()
 
@@ -19,11 +12,8 @@ html = markdown.markdown(
     tab_length=2,
     extensions=['def_list'])
 
-# print(html)
-
-
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
-jinja_template = jinja_env.get_template("template.html")
+jinja_template = jinja_env.get_template("web/template.html")
 jinja_output = jinja_template.render(content=html)
 
 pathlib.Path("target").mkdir(parents=True, exist_ok=True)
