2893889d2c1fab208dbe96deecee96b9c4e2b782
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2020-02-29 13:21:27 +0100

    Added footer with status information

--- generate_html.py
+++ generate_html.py
@@ -3,17 +3,7 @@ import codecs
 import jinja2
 import pathlib
 import time
-
-def render_recursively(template, **kwargs):
-  prev_result = None
-  result = template.render(**kwargs)
-
-  while result != prev_result:
-    prev_result = result
-    template = jinja2.Template(prev_result)
-    result = template.render(**kwargs)
-    
-  return result
+import git
 
 input_file = codecs.open("terminology.md", mode="r", encoding="utf-8")
 text = input_file.read()
@@ -24,18 +14,18 @@ html = markdown.markdown(
     tab_length=2,
     extensions=['def_list', 'admonition'])
 
+local_git_repo = git.Repo(search_parent_directories=True)
+local_git_sha = local_git_repo.head.object.hexsha
+
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
 jinja_template = jinja_env.get_template("web/template.html")
 
-jinja_output = render_recursively(
-  jinja_template, 
+jinja_output = jinja_template.render(
   content=html,
-  last_updated=time.strftime("%Y-%m-%d %H:%M GMT", time.gmtime()))
-
-# jinja_output = jinja_template.render(
-#     content=html)
-
-# jinja_output = jinja2.Template(jinja_output).render(last_updated="NOW")
+  last_updated=time.strftime("%Y-%m-%d %H:%M GMT", time.gmtime()),
+  git_revision=local_git_sha[:10],
+  github_revision_url="https://github.com/mschwerhoff/pp20-terminology/commit/%s" % local_git_sha,
+  github_repository_url="https://github.com/mschwerhoff/pp20-terminology")
 
 pathlib.Path("target").mkdir(parents=True, exist_ok=True)
 
