1207f4fb9a635806a992b45c65443afddf5d31a0
Malte Schwerhoff <malte.schwerhoff@inf.ethz.ch>
2020-02-29 11:55:27 +0100

    [WIP] Experimenting: recursive Jinja template expansion

--- generate_html.py
+++ generate_html.py
@@ -2,6 +2,18 @@ import markdown
 import codecs
 import jinja2
 import pathlib
+import time
+
+def render_recursively(template, **kwargs):
+  prev_result = None
+  result = template.render(**kwargs)
+
+  while result != prev_result:
+    prev_result = result
+    template = jinja2.Template(prev_result)
+    result = template.render(**kwargs)
+    
+  return result
 
 input_file = codecs.open("terminology.md", mode="r", encoding="utf-8")
 text = input_file.read()
@@ -10,11 +22,20 @@ html = markdown.markdown(
     text,
     output_format="html5", 
     tab_length=2,
-    extensions=['def_list'])
+    extensions=['def_list', 'admonition'])
 
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader('./'))
 jinja_template = jinja_env.get_template("web/template.html")
-jinja_output = jinja_template.render(content=html)
+
+jinja_output = render_recursively(
+  jinja_template, 
+  content=html,
+  last_updated=time.strftime("%Y-%m-%d %H:%M GMT", time.gmtime()))
+
+# jinja_output = jinja_template.render(
+#     content=html)
+
+# jinja_output = jinja2.Template(jinja_output).render(last_updated="NOW")
 
 pathlib.Path("target").mkdir(parents=True, exist_ok=True)
 
